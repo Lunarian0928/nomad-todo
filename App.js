d2100ec69d6935ade543329fd173a4396de8941c
[Lunarian0928] <[thanks092834@gmail.com]>
2024-02-20 20:04:55 +0900

    할일/여행지 목록 추가 및 출력 기능 추가 (할일과 여행지 목록을 구분하는 기능 추가 예정)

--- App.js
+++ App.js
@@ -8,17 +8,34 @@ import {
   TouchableHighlight, // 다양한 속성을 변경 가능
   TouchableWithoutFeedback, // UI 반응이 따로 나타나지 않음
   Pressable, // 확장성이 있고 미래에도 사용가능
-  TextInput
+  TextInput,
+  ScrollView,
 } from 'react-native';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { theme } from './colors';
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
+  const addToDo = () => {
+    if (text === "") return;
+    // const newToDos = Object.assign({}, toDos, {
+    //   [Date.now()]: { text, work: working },
+    // });
+    const newToDos = {...toDos, 
+      [Date.now()] : {text, work: working},
+    };
+    setToDos(newToDos);
+    setText("");
+  }
+
+  useEffect(() => {
+    console.log(toDos);
+  }, [toDos])
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
@@ -42,12 +59,21 @@ export default function App() {
       </View>
       <View>
         <TextInput 
+          onSubmitEditing={() => addToDo()}
           onChangeText={(payload) => onChangeText(payload)}
+          returnKeyType="done"
           value={text}
           placeholder={working ? "할 일을 추가하세요" : "가고 싶은 곳을 추가하세요"}
           style={styles.input}
         />
       </View>
+      <ScrollView>
+      { Object.keys(toDos).map(key => (
+        <View style={styles.toDo}>
+          <Text style={styles.toDoText}>{toDos[key].text}</Text>
+        </View>
+      ))}
+      </ScrollView>
     </View>
   );
 }
@@ -72,7 +98,19 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 30,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.toDoBg,
+    marginBottom: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 15,
+  },
+  toDoText: { 
+    color: theme.white,
+    fontSize: 16,
+    fontWeight: "500",
   }
 });
--- colors.js
+++ colors.js
@@ -2,4 +2,5 @@ export const theme = {
     bg: "black",
     white: "#FFF",
     gray: "#3A3D40",
+    toDoBg: "#5C5C60",
 };
